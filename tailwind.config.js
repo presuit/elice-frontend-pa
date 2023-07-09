66f71d3656e9ae121d2886438272dd24a2b66d2a
presuit <chldudgh8256@naver.com>
2023-07-10 01:03:58 +0900

    style:pretendard 폰트 설치 및 color theme extends

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,15 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "main-background": "rgb(240, 241, 243)",
+        "input-background": "rgb(201, 202, 204)",
+        "filtering-title-background": "rgb(249, 250, 252)",
+        "filtering-title-border": "rgb(225, 226, 228)",
+        "filtering-title-text": "#5e5f61",
+      },
+    },
   },
   plugins: [],
 };
