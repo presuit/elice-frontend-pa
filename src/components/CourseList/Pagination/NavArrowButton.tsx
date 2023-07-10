c0c4b415cb13fc76018eb07c549e5fb3907dbe40
presuit <chldudgh8256@naver.com>
2023-07-11 08:38:47 +0900

    refactor:Pagination 화살표 버튼 컴포넌트로 분리

---
+++ src/components/CourseList/Pagination/NavArrowButton.tsx
@@ -0,0 +1,49 @@
+import IcoChevronLeft from "@/components/Icons/IcoChevronLeft";
+import { PAGINATION_MINIMUM_PAGE } from "@/constants/courseCard";
+import classNames from "classnames";
+import { Dispatch, SetStateAction } from "react";
+
+interface Props {
+  direction: "LEFT" | "RIGHT";
+  page: number;
+  setPage: Dispatch<SetStateAction<number>>;
+  maxPage: number;
+}
+
+export default function NavArrowButton({
+  maxPage,
+  page,
+  setPage,
+  direction,
+}: Props) {
+  function handleClickNextPage() {
+    const nextPage = Math.min(maxPage, page + 1);
+    setPage(nextPage);
+  }
+  function handleClickPrevPage() {
+    const prevPage = Math.max(PAGINATION_MINIMUM_PAGE, page - 1);
+    setPage(prevPage);
+  }
+
+  const isFirstPage = direction === "LEFT" && page === PAGINATION_MINIMUM_PAGE;
+  const isLastPage = direction === "RIGHT" && page === maxPage;
+
+  const ARROW_BASE_STYLE =
+    "w-6 aspect-square text-[#222] rounded-[.25rem] py-[1px] px-[6px] text-sm flex justify-center items-center";
+  const ICON_BASE_STYLE = "stroke-[3] w-[14px] h-[14px] shrink-0";
+
+  return (
+    <button
+      onClick={direction === "LEFT" ? handleClickPrevPage : handleClickNextPage}
+      className={classNames(ARROW_BASE_STYLE, {
+        "text-[#ccc] cursor-not-allowed": isFirstPage || isLastPage,
+      })}
+    >
+      <IcoChevronLeft
+        classname={classNames(ICON_BASE_STYLE, {
+          "rotate-180": direction === "RIGHT",
+        })}
+      />
+    </button>
+  );
+}
