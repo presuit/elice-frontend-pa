3e84e8bea7be10f2af7282597c90043d01eb15cb
presuit <chldudgh8256@naver.com>
2023-07-10 19:39:26 +0900

    chore:sr-only 문구 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import CourseList from "@/components/CourseList";
 import FilterChips from "@/components/FilterChips";
 import SearchInput from "@/components/SearchInput";
 
@@ -7,15 +8,16 @@ export default function Home() {
       <main className="p-6 mx-auto max-w-7xl w-full">
         {/* filtering */}
         <section>
-          <h2 className="sr-only">Course Filtering</h2>
+          <h2 className="sr-only">과목 필터링</h2>
           <div>
             <SearchInput />
             <FilterChips />
           </div>
         </section>
         {/* contents */}
-        <section className="p-5 border border-input-background mt-5">
-          <h2 className="sr-only">Course Contents</h2>
+        <section className="mt-3">
+          <h2 className="sr-only">과목 리스트</h2>
+          <CourseList />
         </section>
       </main>
     </div>
